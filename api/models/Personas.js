cd76fc248585ca7de2531bc57f20a68015cd85df
AaronRojas-579 <aaron.rojaas@outlook.com>
2023-02-13 22:46:22 -0300

    first commit

---
+++ api/models/Personas.js
@@ -0,0 +1,17 @@
+/**
+ * Personas.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    nombre:{type:"string",required:true},
+    apellido:{type:"string",required:true},
+    edad:{type:"number",required:true},
+  },
+
+};
+
